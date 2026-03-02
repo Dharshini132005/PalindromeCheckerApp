96203a5d02043574bd83256b7ef59710456ffd72
Dharshini132005 <udharshini1392005@gmail.com>
2026-03-02 15:15:40 +0530

    UC2: Hardcoded palindrome validation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,31 @@
-public class UseCase1PalindromeCheckerApp {
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        System.out.println("Version : 2.0");
+
+        // Hardcoded string
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        // Compare characters from both ends
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
+
+        System.out.println("Program execution completed.");
     }
 }
